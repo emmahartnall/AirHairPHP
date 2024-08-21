a5631ff890fe8c6cc4cb225abcde005820bdd1ea
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-08-21 22:12:09 +1000

    get config working

--- public/display_records.php
+++ public/display_records.php
@@ -1,5 +1,11 @@
 <?php
-include '../src/Model/db_connect.php'; // Include the database connection
+require_once '../config/config.php';
+
+$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $sql = "SELECT * FROM customer";
 $result = $conn->query($sql);
